27c6d1fa795ea9866419264339ef220ef3338b80
nestorhugo <nestorhugo@proton.me>
2024-11-01 10:23:07 -0300

    :refactor: delete redundant code

--- setup.sh
+++ setup.sh
@@ -6,23 +6,13 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Verifica se está rodando no Bash ou Zsh e carrega o arquivo correto
-if [ -n "$BASH_VERSION" ]; then
-    echo "Carregando ~/.bashrc..."
-    source ~/.bashrc
-elif [ -n "$ZSH_VERSION" ]; then
-    echo "Carregando ~/.zshrc..."
-    source ~/.zshrc
-else
-    echo "Shell não identificado. Carregue o NVM manualmente se necessário."
-fi
-
 echo "Instalando a versão LTS do Node.js..."
 nvm install --lts
 
 echo "Instalando TypeScript..."
 npm install -g typescript
 
-echo "Instalação concluída!"
+echo "Carregando ~/.bashrc..."
+source ~/.bashrc
 
-exec "$SHELL"
+echo "Instalação concluída!"
